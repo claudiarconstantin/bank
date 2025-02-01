7eeb2cc0fb51bf3fed58dd93dfaf766496402121
claudiarconstantin <claudiar.constantin@gmail.com>
2025-02-01 14:35:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from PyBank import Bank, Person
 
 bank = Bank("Banca Transilvania")
 
-persoana_1 = Person("Ionescu", "Cristi", 123456789)
-persoana_2 = Person("Popescu", "Andrei", 234567891)
-persoana_3 = Person("Dumitru", "Elena", 345678912)
-persoana_4 = Person("Georgescu", "Mihai", 456789123)
-persoana_5 = Person("Mihaescu", "Marin", 987654321)
+persons = [
+    Person("Ionescu", "Cristi", 123456789),
+    Person("Popescu", "Andrei", 234567891),
+    Person("Dumitru", "Elena", 345678912),
+    Person("Georgescu", "Mihai", 456789123),
+    Person("Mihaescu", "Marin", 987654321)
+]
 
-bank.register_employee(persoana_1)
-bank.register_employee(persoana_2)
-bank.register_employee(persoana_3)
-bank.register_employee(persoana_4)
-bank.register_employee(persoana_5)
+for person in persons:
+    bank.register_employee(person)
 
 bank.print_all_employee()
